fda3d95e7ad228d942205b5c6ad963c5a7551a0c
Akhlaque Ahmad <akhlaquea01@gmail.com>
2023-11-24 10:47:04 +0530

    controller and router setup

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
-const app = express();
+const app = express()
+
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
-}));
+}))
 
-app.use(express.json({ limit: "16kb" })); //Accept JSON till 16kb limit
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public")); //To store data on server temp
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
 
-app.use(cookieParser());
 
-export { app };
+//routes import
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
+
+export { app }
