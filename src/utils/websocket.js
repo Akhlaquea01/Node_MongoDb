7c46348a87894e16680287ea8609963333c56d32
Akhlaque <akhlaquea01@gmail.com>
2024-05-02 21:39:44 +0530

    initial socket connect stablished

---
+++ src/utils/websocket.js
@@ -0,0 +1,32 @@
+import { Server } from "socket.io";
+import http from "http";
+
+export function setupWebSocketServer(app) {
+    const server = http.createServer(app);
+
+    // Configure CORS
+    const io = new Server(server, {
+        cors: {
+            origin: process.env.SOCKET_ORIGIN,
+            // methods: ["GET", "POST"],
+            credentials: true
+        }
+    });
+
+    io.on("connection", (socket) => {
+        console.log("A user connected");
+
+        // Handle chat message event
+        socket.on("chat message", (msg) => {
+            console.log("message: " + msg);
+            io.emit("chat message", msg); // Broadcast message to all connected clients
+        });
+
+        // Handle disconnect event
+        socket.on("disconnect", () => {
+            console.log("User disconnected");
+        });
+    });
+
+    return { server, io };
+}
