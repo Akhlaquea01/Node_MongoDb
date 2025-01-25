951abdbe9c070a6cc33196a3f2d0d61df7670c5d
Akhlaque <akhlaquea01@gmail.com>
2025-01-25 19:46:49 +0530

    Accounts Api's created

--- src/controllers/bank.controller.js
+++ src/controllers/bank.controller.js
@@ -36,9 +36,70 @@ const createAccount = asyncHandler(async (req, res) => {
     }
 
 });
+const updateAccount = asyncHandler(async (req, res) => {
+    try {
+        const { accountId } = req.params;
+        const { accountName, accountType, balance, isDefault, status } = req.body;
+
+        // Check if the account is being set as default and unset other default accounts for the user
+        if (isDefault) {
+            const account = await Account.findById(accountId);
+            if (account) {
+                await Account.updateMany({ userId: account.userId, isDefault: true }, { isDefault: false });
+            }
+        }
+
+        const updatedAccount = await Account.findByIdAndUpdate(
+            accountId,
+            { accountName, accountType, balance, isDefault, status },
+            { new: true }
+        );
+
+        if (!updatedAccount) {
+            throw new ApiError(400, "Account not found");
+        }
+        return res.status(201).json(
+            new ApiResponse(200, { account: updatedAccount }, "Account updated successfully")
+        );
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while creating the account", error.message);
+    }
+
+});
+
+const deleteAccount = asyncHandler(async (req, res) => {
+    try {
+        const { accountId } = req.params;
+
+        const deletedAccount = await Account.findByIdAndDelete(accountId);
+
+        if (!deletedAccount) {
+            throw new ApiError(404, "Account not found");
+        }
+        return res.status(201).json(
+            new ApiResponse(200, { message: "Account deleted successfully" }, "Account deleted successfully")
+        );
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while creating the account", error.message);
+    }
+
+});
+const getAccount = asyncHandler(async (req, res) => {
+    try {
+        const { userId } = req.query;
+
+        const accounts = await Account.find({ userId });
+        return res.status(201).json(
+            new ApiResponse(200, { accounts }, "Account deleted successfully")
+        );
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while creating the account", error.message);
+    }
+
+});
 
 
 export {
-    createAccount,
+    createAccount, updateAccount, deleteAccount, getAccount
 
 };
--- src/routes/bank.routes.js
+++ src/routes/bank.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import {
-    createAccount,
+    createAccount, updateAccount, deleteAccount, getAccount
 } from "../controllers/bank.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
@@ -8,7 +8,9 @@ const router = Router();
 
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/").post(createAccount);
-// .get(getAccount)
+router.route("/create").post(createAccount);
+router.route("/:accountId").put(updateAccount);
+router.route("/:accountId").delete(deleteAccount);
+router.route("/get").get(getAccount)
 
 export default router
